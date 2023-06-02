b1f45f301deb9bb18f7e18c2c59d6213c1e1f614
Ovais Mehboob Ahmed Khan <Ovais.Mehboob@microsoft.com>
2023-06-02 11:00:48 +0400

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace testapplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
